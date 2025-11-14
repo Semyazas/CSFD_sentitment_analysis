587a6f22f36f1646cb64fd0c89575f5f851da961
Martin Kunst <martin.kunst2001@centrum.cz>
2025-11-14 17:28:32 +0100

    refactor(Collect_movies_data.py): TODO: make data collection on fly + make it so that even if some review could not be scrapped we continue from the same page

--- collect_data/Collect_movies_data.py
+++ collect_data/Collect_movies_data.py
@@ -5,8 +5,8 @@ from Scrappers.UrlFinder import UrlFinder
 def collect_data_for_movies_from_genres(
         genres: list[str],
         conn : sqlite3.Connection,
-        number_of_movies_from_genre : int  = 5,
-        number_of_pages_to_scroll : int = 5) -> None:
+        number_of_movies_from_genre : int  = 10,
+        number_of_pages_to_scroll : int = 10) -> None:
     url_scrapper = UrlFinder()
     movies = url_scrapper.scrap_url_from_genres(
         genres,
@@ -16,7 +16,7 @@ def collect_data_for_movies_from_genres(
     movie_scrapper = MovieReviewScraper()
     for i,movie_url in enumerate(movies):
         genre = genres[i // number_of_movies_from_genre]
-        print(f"Collecting data for movie: {movie_url} of genre {genre}")
+        print(f"Collecting data for movie: {movie_url} of genre {genre}, number_of_pages_to_scroll: {number_of_pages_to_scroll} ")
         collect_data_for_movie(
             movie_url,
             conn,
@@ -24,21 +24,9 @@ def collect_data_for_movies_from_genres(
             genre[0],
             scrapper= movie_scrapper            
         )
-    
-def collect_data_for_movie(
-        movie_url: str,
+def add_data_to_reviews_table(
         conn : sqlite3.Connection,
-        number_of_pages : int,
-        genre : str,
-        scrapper: MovieReviewScraper) -> None:
-    data = scrapper.\
-        scrap_CSFD_reviews_from_movie(movie_url + "recenze/", number_of_pages)
-
-    movies_data, avg_rating = data["reviews"], data["average_rating"]
-    print(f"Average rating for {movie_url}: {avg_rating}")
-    movie = movie_url.split("/")[-2].split("-")
-    movie = " ".join([word.capitalize() for word in movie[1:]])
-    print(f"Movie name extracted: {movie} genre: {genre} ")
+        reviews_data : list[tuple[str, str, str, str, str]]) -> None:
     c = conn.cursor()
     # username, rating, review_text, movie_name
     # Create a table
@@ -47,33 +35,63 @@ def collect_data_for_movie(
             username,
             rating, 
             review_text, 
-            movie_name
-        ) VALUES (?, ?, ?, ?)
+            movie_name,
+            user_ref
+        ) VALUES (?, ?, ?, ?, ?)
         """, 
-        movies_data
+        reviews_data
     )
-    print(c.fetchall())
     conn.commit()
 
+def add_data_to_movies_table(
+        conn : sqlite3.Connection,
+        movie_name : str,
+        genre : str,
+        average_rating : float) -> None:
+    c = conn.cursor()
     c.execute("""
         INSERT INTO movies (
             movie_name,
             genre,
             average_rating
         ) VALUES (?, ?, ?)""",
-        (movie, genre, avg_rating))
+        (movie_name, genre, average_rating))
     conn.commit()
+        
+
+def collect_data_for_movie(
+        movie_url: str,
+        conn : sqlite3.Connection,
+        number_of_pages : int,
+        genre : str,
+        scrapper: MovieReviewScraper) -> None:
+
+    data = scrapper.\
+        scrap_CSFD_reviews_from_movie(movie_url[:-8] + "recenze/", number_of_pages)
+
+    movies_data, avg_rating = data["reviews"], data["average_rating"]
+    print(f"Average rating for {movie_url}: {avg_rating}")
+    movie = movies_data[0][3]
+    print(f"Movie name extracted: {movie} genre: {genre} ")
+    
+    add_data_to_reviews_table(conn, movies_data)
+    add_data_to_movies_table(conn, movie, genre, avg_rating)
+
 def main():
     conn = sqlite3.connect('movie_reviews.db')
     genres = [
-
-        ("Thriller",False)
+        ("Thriller",False),
+        ("Komedie",False),
+        ("Drama",False),
+        ("Horor",False),
+        ("Sci-Fi",False),
+        ("Fantasy",False)
     ]
     collect_data_for_movies_from_genres(
         genres,
         conn,
-        number_of_movies_from_genre = 5,
-        number_of_pages_to_scroll = 5
+        number_of_movies_from_genre = 10,
+        number_of_pages_to_scroll = 10
     )
     conn.close()
 if __name__ == "__main__":
