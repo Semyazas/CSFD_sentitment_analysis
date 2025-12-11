90f4dc29198598c58d541bd4fbeda1b4afcc117a
Martin Kunst <martin.kunst2001@centrum.cz>
2025-12-11 22:06:42 +0100

    perf(Collect_user_data): made collecting user data parallel

--- collect_data/Collect_user_data.py
+++ collect_data/Collect_user_data.py
@@ -1,5 +1,17 @@
 import sqlite3
 from Scrappers.UserProfileScrapper import UserProfileScrapper 
+from multiprocessing import Process
+
+def process_batch(data_batch):
+    # create resources inside the process
+    conn = sqlite3.connect('movie_reviews.db')
+    c = conn.cursor()
+    scraper = UserProfileScrapper(cursor=c)
+
+    scraper.get_user_data(data_batch)
+
+    conn.commit()
+    conn.close()
 
 def main() -> None:    
     conn = sqlite3.connect('movie_reviews.db')
@@ -15,11 +27,21 @@ def main() -> None:
     data = c.fetchall()
     print("Number of users to scrape:", len(data))
     print(data[0])
-
     conn.commit()
+    processes = []
+    batch_size = len(data) // 3
 
-    Scrapper = UserProfileScrapper()
-    Scrapper.get_user_data(data,c)
+    for i in range(0, len(data), batch_size):
+        data_batch = data[i:i + batch_size]
+        print(f"Starting batch {i} to {i + batch_size}")
+
+        p = Process(target=process_batch, args=(data_batch,))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+    
 
 if __name__ == "__main__":
     main()
